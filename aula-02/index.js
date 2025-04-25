2eaea8f2b612afce5a625fad12dfc1dda29ad058
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-25 07:27:07 -0300

    aula 02

---
+++ aula-02/index.js
@@ -0,0 +1,7 @@
+// Lógica de programação é saber como criar algoritmos
+// Algoritmos nada mais é do que pensar em soluções passo a passo para resolver o problema
+// determinado.
+
+let nome = "Chagas" // String
+let idade = 33 // number
+let fumante = false // boolean
