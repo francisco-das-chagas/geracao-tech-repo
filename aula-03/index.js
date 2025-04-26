bd00320f534d5d223cb29a0b42086d408ea43fe8
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-25 21:35:37 -0300

    Aula 5

--- aula-03/index.js
+++ aula-03/index.js
@@ -1,4 +1,7 @@
-let numero1 = 2;
-let numero2 = 3;
+let numero1 = Number(prompt('Digite o primeiro número'));
+let numero2 = Number(prompt('Digite o segundo número'));
 let resultado = numero1 + numero2;
-alert(resultado)
+// alert(resultado)
+document.write('O resultado é: '+ resultado)
+
+
