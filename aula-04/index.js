08097c652a3af1369fca6daa33d63a1c40b6b41d
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-27 22:02:47 -0300

    aula 9

--- aula-04/index.js
+++ aula-04/index.js
@@ -8,12 +8,26 @@
 // boasVindas('Alessandro');
 // boasVindas('Sarah');
 
-function somar(numero1, numero2) {
-  // document.write(`<h1>A soma é: ${numero1 + numero2} </h1>`)
-  console.log(numero1 + numero2)
-  return numero1 + numero2
-}
-somar(10002, 121212)
-somar(65, 13)
+// function somar(numero1, numero2) {
+//   // document.write(`<h1>A soma é: ${numero1 + numero2} </h1>`)
+//   console.log(numero1 + numero2)
+//   return numero1 + numero2
+// }
+// somar(10002, 121212)
+// somar(65, 13)
 
-document.write(`<h1>O resultado é: ${somar(1,1)}</h1>`)
+// document.write(`<h1>O resultado é: ${somar(1,1)}</h1>`)
+
+// function somar_exemplo(a, b) {
+//   return a + b;
+// }
+// let resultado = somar_exemplo(5, 3);
+// console.log(resultado); // 
+
+function preencherSelects(tag, limite) {
+  for ( let i = 1; i < limite; i++) {
+    tag.innerHTML += `<option>${i}</option>`
+  }
+}
+preencherSelects(dia, 32)
+preencherSelects(mes, 13)
