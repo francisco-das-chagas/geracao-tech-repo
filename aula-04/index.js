c8682b07a7d58d711e166260fc94efc2f679385a
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-27 21:39:40 -0300

    aula 8

--- aula-04/index.js
+++ aula-04/index.js
@@ -1,9 +1,19 @@
-// Funções são trecho de códigos repetiveis
+// // Funções são trecho de códigos repetiveis que pode ou não ter parâmetros e pode ou não ter retorno nem toda função vai precisar ter retorno
 
-function boasVindas(nome) {
-  document.write(`<h1>Olá ${nome}</h1>`);
+// function boasVindas(nome) {
+//   document.write(`<h1>Olá ${nome}</h1>`);
+// }
+// boasVindas('Gleidson');
+// boasVindas('Márcio');
+// boasVindas('Alessandro');
+// boasVindas('Sarah');
+
+function somar(numero1, numero2) {
+  // document.write(`<h1>A soma é: ${numero1 + numero2} </h1>`)
+  console.log(numero1 + numero2)
+  return numero1 + numero2
 }
-boasVindas('Gleidson');
-boasVindas('Márcio');
-boasVindas('Alessandro');
-boasVindas('Sarah');
+somar(10002, 121212)
+somar(65, 13)
+
+document.write(`<h1>O resultado é: ${somar(1,1)}</h1>`)
