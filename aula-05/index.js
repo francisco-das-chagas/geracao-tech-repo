cd340b1ed513f86ac93a7c6a9712630b7eaed451
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-28 22:40:35 -0300

    iniciando aula 9

---
+++ aula-05/index.js
@@ -0,0 +1,14 @@
+let pessoa = {
+    nome: 'Gleidson',
+    idade: 22,
+    genero: 'masculino',
+    fumante: false,
+    habilidades: ['HTML', 'CSS', 'Javascript', 'React'],
+    andar: function outroNome () {
+      console.log('Andei');
+    }
+} 
+
+let subtrair = function () {
+  console.log('subtrair');
+}
