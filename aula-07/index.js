3d91c2aba0335479fc5c9ed92085a181f90d6d3a
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-30 19:13:06 -0300

    adicionando aula 11

---
+++ aula-07/index.js
@@ -0,0 +1,17 @@
+// == : igual a
+// != : diferente de
+// > : maior que
+// < : menor que
+// >= : maior ou igual a
+// <= : menor ou igual a
+let numero1 = Number(prompt('Numero 1'))
+// let numero2 = Number(prompt('Numero 2'))
+
+if (numero1 > 5) {
+  console.log('É maior que 5');
+} else if (numero1 <5 ){
+  console.log('Não é maior que 5');
+}else {
+  console.log('É igual a 5');
+}
+
