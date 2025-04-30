e0b1d432382fcef751f14ff5e2d205728b907190
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-04-30 19:45:37 -0300

    aula 11 finalizada

--- aula-08/index.js
+++ aula-08/index.js
@@ -1,1 +1,13 @@
-  
+let mode = "green";
+
+switch (mode) {
+  case "dark":
+    document.querySelector("body").style.background = "#222";
+    break;
+  case "red":
+    document.querySelector("body").style.background = "#ff0000";
+    break;
+  case "blue":
+    document.querySelector("body").style.background = "#0000ff";
+    break;
+}
