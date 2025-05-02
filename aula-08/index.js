2a2b12134c7627bf141b40f09e537f6f209a4414
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-01 23:03:51 -0300

    finalizando aula 11

--- aula-08/index.js
+++ aula-08/index.js
@@ -1,13 +1,15 @@
-let mode = "green";
+let mode = 'dark'
 
-switch (mode) {
-  case "dark":
-    document.querySelector("body").style.background = "#222";
-    break;
-  case "red":
-    document.querySelector("body").style.background = "#ff0000";
-    break;
-  case "blue":
-    document.querySelector("body").style.background = "#0000ff";
-    break;
-}
+
+console.log(mode == 'dark' ? 'É' : 'Não');
+// switch (mode) {
+//   case "dark":
+//     document.querySelector("body").style.background = "#222";
+//     break;
+//   case "red":
+//     document.querySelector("body").style.background = "#ff0000";
+//     break;
+//   case "blue":
+//     document.querySelector("body").style.background = "#0000ff";
+//     break;
+// }
