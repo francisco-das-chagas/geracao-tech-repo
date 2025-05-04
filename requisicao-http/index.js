9b912b28509cd308797b7ddc375d8c42db1346f9
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-04 19:46:54 -0300

    aula de requisição

--- requisicao-http/index.js
+++ requisicao-http/index.js
@@ -1,4 +1,9 @@
   cep.onkeyup = () => {
-    console.log('apertou');
-    // if(){}
+    if(cep.value.length == 8){
+      fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+      .then(resposta => resposta.json())
+      .then(resposta2 => {
+        console.log(resposta2);
+      })
+    }
   }
