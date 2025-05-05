62af0a5f1df92ac3e7e0f7d40b3038320c12c1e9
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-05 20:23:29 -0300

    finalizando exercicio 1 de consumo de API

--- requisicao-http/index.js
+++ requisicao-http/index.js
@@ -3,7 +3,10 @@
       fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
       .then(resposta => resposta.json())
       .then(resposta2 => {
-        console.log(resposta2);
+        rua.value = resposta2.logradouro;
+        bairro.value = resposta2.bairro;
+        cidade.value = resposta2.localidade;
+        estado.value = resposta2.uf;
       })
     }
   }
