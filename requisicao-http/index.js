290b710a94aabc81b9b03ca1dad41c2da430433a
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-06 22:20:36 -0300

    finalizando aula endpoint

--- requisicao-http/index.js
+++ requisicao-http/index.js
@@ -12,5 +12,36 @@
   // }
 
   function buscarRegioes() {
-    fetch
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes")
+    .then( res => res.json())
+    .then( res => {
+      res.map(regiao => {
+        regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`
+      })
+    })
   }
+  buscarRegioes()
+  
+  function buscarEstados() {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`)
+    .then( res => res.json())
+    .then( res => {
+      estados.innerHTML = "";
+      res.map(estado => {
+        estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`
+      })
+    })
+  }
+  buscarEstados()
+
+  function buscarCidades() {
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estados.uf}/municipios`)
+    .then( res => res.json())
+    .then( res => {
+      estados.innerHTML = "";
+      res.map(cidade => {
+        cidades.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`
+      })
+    })
+  }
+  buscarCidades()
