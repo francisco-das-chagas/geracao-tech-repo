33fa9170976294059a8f2442d46094ce76c28567
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-06 21:48:22 -0300

    iniciando aula de endpoints

--- requisicao-http/index.js
+++ requisicao-http/index.js
@@ -1,12 +1,16 @@
-  cep.onkeyup = () => {
-    if(cep.value.length == 8){
-      fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
-      .then(resposta => resposta.json())
-      .then(resposta2 => {
-        rua.value = resposta2.logradouro;
-        bairro.value = resposta2.bairro;
-        cidade.value = resposta2.localidade;
-        estado.value = resposta2.uf;
-      })
-    }
+  // cep.onkeyup = () => {
+  //   if(cep.value.length == 8){
+  //     fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+  //     .then(resposta => resposta.json())
+  //     .then(resposta2 => {
+  //       rua.value = resposta2.logradouro;
+  //       bairro.value = resposta2.bairro;
+  //       cidade.value = resposta2.localidade;
+  //       estado.value = resposta2.uf;
+  //     })
+  //   } 
+  // }
+
+  function buscarRegioes() {
+    fetch
   }
