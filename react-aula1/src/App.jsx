7794277b96e83d7f1d127864f91289e72fd72543
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-08 20:50:05 -0300

    finalizando aula 1

--- react-aula1/src/App.jsx
+++ react-aula1/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hello dev
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const Hello = () => {
+  return(
+    <h1>Hello Dev</h1>
   )
 }
 
-export default App
+
+const Paragrafo = () => {
+  return(
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. asdasdsada</p>
+  )
+}
+const App = () => {
+  // Parte lógica do componente
+  return(
+    // Parte visivel para o usuário
+    <Hello/>
+  )
+}
+
+
+
+
+export default App;
