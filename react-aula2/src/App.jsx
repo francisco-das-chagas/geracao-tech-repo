e07cb3deb23bbd41fdd3362e553b88c160ce4b39
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-12 23:39:32 -0300

    aula 3

--- react-aula2/src/App.jsx
+++ react-aula2/src/App.jsx
@@ -1,4 +1,5 @@
 import Banner from "./components/banner";
+import FormaDeBolo from "./components/FormaDeBolo";
 import Header from "./components/header";
 import { Componente1,  Componente2 } from "./components/VariosComponents";
 
@@ -6,10 +7,12 @@ const App = () => {
   return ( 
     // Parte visivel do cmponente
     <>
-      <Header />
+      {/* <Header />
       <Banner />
       <Componente1 />
-      <Componente2 />
+      <Componente2 /> */}
+      <FormaDeBolo sabor = "laranja" cobertura = "Morango" />
+      <FormaDeBolo sabor = "Chocolate" cobertura = "Chantily" />
     </>
    );
 }
