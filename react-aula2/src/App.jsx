a511e0d3f2291876750be17d7e04d124bb683937
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-14 22:43:31 -0300

    adicionando aula 3 react

--- react-aula2/src/App.jsx
+++ react-aula2/src/App.jsx
@@ -1,5 +1,6 @@
 import Banner from "./components/banner";
 import Card from "./components/Card";
+import CardDinamico from "./components/CardDinamico";
 import FormaDeBolo from "./components/FormaDeBolo";
 import Header from "./components/header";
 import { Componente1,  Componente2 } from "./components/VariosComponents";
@@ -9,14 +10,36 @@ const App = () => {
     // Parte visivel do cmponente
     <>
       <Card 
+        image={"https://sesies.com.br/wp-content/uploads/2023/02/GettyImages-497811221-1024x681.jpg"}
         title={"Noticia 1"} 
-        category = {"Esportes"} 
-        paragraph= {"Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium reprehenderit unde rerum corporis omnis nobis natus consectetur provident rem nam dolorum eos delectus obcaecati, ducimus temporibus doloribus molestiae, totam impedit."}
+        category={"Esportes"} 
+        paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aliquam magni dolorum excepturi. At, error et repellendus dicta sunt nesciunt blanditiis, ab rem explicabo aliquam, pariatur magnam ullam dolorum praesentium."}
+        tipo={"A"}
       />
+      <Card 
+        image={"https://sesies.com.br/wp-content/uploads/2023/02/GettyImages-497811221-1024x681.jpg"}
+        title={"Noticia 2"} 
+        category={"Finanças e Investimento"} 
+        paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aliquam magni dolorum excepturi. At, error et repellendus dicta sunt nesciunt blanditiis, ab rem explicabo aliquam, pariatur magnam ullam dolorum praesentium."}
+        tipo={"B"}
+      />
+      <Card 
+        image={"https://sesies.com.br/wp-content/uploads/2023/02/GettyImages-497811221-1024x681.jpg"}
+        title={"Noticia 3"} 
+        category={"Culinária"} 
+        paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aliquam magni dolorum excepturi. At, error et repellendus dicta sunt nesciunt blanditiis, ab rem explicabo aliquam, pariatur magnam ullam dolorum praesentium."}
+        tipo={"C"}
+      />
+      <CardDinamico>
+        <h1>Noticia 4</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsLjXFUZj-jZdd_sQnlNblsK6raz6t19ZsAg&s" alt="" />
+        <h6>Categoria</h6>
+      </CardDinamico>
     </>
    );
 
 }
+
  
 export default App;
 
--- react-aula2/src/components/Card.jsx
+++ react-aula2/src/components/Card.jsx
@@ -1,9 +1,17 @@
-const Card = (title, category, paragraph) => {
+const Card = ({image, title, category, paragraph, tipo}) => {
   return ( 
     <div style={{border: "1px solid black"}}>
+      <img src={image} alt ={title} />
       <h2>{title}</h2>
-      <h6>{category}</h6>
-      <p>{paragraph}</p>
+      {
+        tipo == "A" && (
+          <>
+            <h6>{category}</h6>
+            <p>{paragraph}</p>
+          </>
+        )
+      }
+      
     </div>
    );
 }
