b7bfebd17e987ef70c020f26d22b5af96f9048da
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-20 22:23:44 -0300

    finalizando aula 5

--- react-aula4/src/Routes/Paths.jsx
+++ react-aula4/src/Routes/Paths.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
 import Produtos from "../../pages/Produtos";
+import Header from "../components/Header";
 
 const Paths = () => {
   return ( 
     <>
         <BrowserRouter>
+          <Header />
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/produtos" element={<Produtos/>}/>
