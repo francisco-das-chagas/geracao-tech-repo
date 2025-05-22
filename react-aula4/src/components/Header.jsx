86f8265838bc7d9ba082acc2438021ea074edc23
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-21 22:46:51 -0300

    finalizando aula 13

--- react-aula4/src/components/Header.jsx
+++ react-aula4/src/components/Header.jsx
@@ -1,12 +1,29 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
   background-color: blueviolet;
-  padding: 0 60px;
+  padding: 10px 60px;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+  & h1 {
+    color: #FFF;
+  }
+  & nav ul {
+    display: flex;
+    gap: 36px;
+    list-style: none;
+    & li a {
+      color: #FFFFFF80;
+      text-decoration: none;
+      font-size: 18px;
+      &:hover, &.active {
+        color: #fff;
+      }
+    }
+  }
 
 `
 const Header = () => {
@@ -15,8 +32,8 @@ const Header = () => {
             <h1>Logo</h1>
             <nav>
               <ul>
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/produtos"}>Produtos</Link></li>
+                <li><NavLink to={"/"}>Home</NavLink></li>
+                <li><NavLink to={"/produtos"}>Produtos</NavLink></li>
               </ul>
             </nav>
     </HeaderContainer>
