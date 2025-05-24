586cfb63396c565cd82efb4553455d8e42686a92
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-23 23:58:58 -0300

    finalizando estrutura login

--- react-aula5/src/pages/Login.jsx
+++ react-aula5/src/pages/Login.jsx
@@ -1,7 +1,34 @@
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+
 const Login = () => {
   return ( 
     <>
-        <h1>Login</h1>
+        <div>
+          <form>
+            <h3>Seja Bem-vindo</h3>
+            <label htmlFor="email">Email</label>
+            <InputText
+              id='email'
+              type='email'
+              placeholder='email@email.com'/>
+            <label htmlFor="senha">Senha</label>
+            <IconField>
+                  <InputIcon InputIcon className="pi pi-eye"> </InputIcon>
+                  <InputText
+                    id='senha'
+                    placeholder='********'
+                  />
+            </IconField>
+            <Button 
+              label="Entrar"
+              type='submit'
+            />
+
+          </form>
+        </div>
     </>
    );
 }
